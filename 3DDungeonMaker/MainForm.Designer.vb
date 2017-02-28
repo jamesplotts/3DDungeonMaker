41a34b176cb73527b7ee3570da552e4ced8e36ee
jamesplotts <jamesplotts@ymail.com>
2017-02-28 00:48:12 -0600

    Added TabControl

--- 3DDungeonMaker/MainForm.Designer.vb
+++ 3DDungeonMaker/MainForm.Designer.vb
@@ -28,106 +28,148 @@ Partial Class MainForm
         Me.ToolStripContainer1 = New System.Windows.Forms.ToolStripContainer()
         Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
         Me.SplitContainer2 = New System.Windows.Forms.SplitContainer()
-        Me.pctSurface = New System.Windows.Forms.PictureBox()
-        Me.ToolStripContainer1.ContentPanel.SuspendLayout()
-        Me.ToolStripContainer1.SuspendLayout()
-        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SplitContainer1.Panel1.SuspendLayout()
-        Me.SplitContainer1.Panel2.SuspendLayout()
-        Me.SplitContainer1.SuspendLayout()
-        CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SplitContainer2.SuspendLayout()
-        CType(Me.pctSurface, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
-        '
-        'MenuStrip1
-        '
-        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
-        Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(970, 24)
-        Me.MenuStrip1.TabIndex = 0
-        Me.MenuStrip1.Text = "MenuStrip1"
-        '
-        'ToolStripContainer1
-        '
-        '
-        'ToolStripContainer1.ContentPanel
-        '
-        Me.ToolStripContainer1.ContentPanel.Controls.Add(Me.SplitContainer1)
-        Me.ToolStripContainer1.ContentPanel.Size = New System.Drawing.Size(970, 378)
-        Me.ToolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.ToolStripContainer1.Location = New System.Drawing.Point(0, 24)
-        Me.ToolStripContainer1.Name = "ToolStripContainer1"
-        Me.ToolStripContainer1.Size = New System.Drawing.Size(970, 403)
-        Me.ToolStripContainer1.TabIndex = 1
-        Me.ToolStripContainer1.Text = "ToolStripContainer1"
-        '
-        'SplitContainer1
-        '
-        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
-        Me.SplitContainer1.Name = "SplitContainer1"
-        '
-        'SplitContainer1.Panel1
-        '
-        Me.SplitContainer1.Panel1.Controls.Add(Me.pctSurface)
-        '
-        'SplitContainer1.Panel2
-        '
-        Me.SplitContainer1.Panel2.Controls.Add(Me.SplitContainer2)
-        Me.SplitContainer1.Size = New System.Drawing.Size(970, 378)
-        Me.SplitContainer1.SplitterDistance = 743
-        Me.SplitContainer1.TabIndex = 0
-        '
-        'SplitContainer2
-        '
-        Me.SplitContainer2.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.SplitContainer2.Location = New System.Drawing.Point(0, 0)
-        Me.SplitContainer2.Name = "SplitContainer2"
-        Me.SplitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal
-        Me.SplitContainer2.Size = New System.Drawing.Size(223, 378)
-        Me.SplitContainer2.SplitterDistance = 168
-        Me.SplitContainer2.TabIndex = 0
-        '
-        'pctSurface
-        '
-        Me.pctSurface.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.pctSurface.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.pctSurface.Location = New System.Drawing.Point(0, 0)
-        Me.pctSurface.Name = "pctSurface"
-        Me.pctSurface.Size = New System.Drawing.Size(743, 378)
-        Me.pctSurface.TabIndex = 0
-        Me.pctSurface.TabStop = False
-        '
-        'MainForm
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(970, 427)
-        Me.Controls.Add(Me.ToolStripContainer1)
-        Me.Controls.Add(Me.MenuStrip1)
-        Me.MainMenuStrip = Me.MenuStrip1
-        Me.Name = "MainForm"
-        Me.Text = "3D Dungeon Maker"
-        Me.ToolStripContainer1.ContentPanel.ResumeLayout(False)
-        Me.ToolStripContainer1.ResumeLayout(False)
-        Me.ToolStripContainer1.PerformLayout()
-        Me.SplitContainer1.Panel1.ResumeLayout(False)
-        Me.SplitContainer1.Panel2.ResumeLayout(False)
-        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.SplitContainer1.ResumeLayout(False)
-        CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.SplitContainer2.ResumeLayout(False)
-        CType(Me.pctSurface, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
+            Me.TabControl1 = New System.Windows.Forms.TabControl()
+            Me.TabPage1 = New System.Windows.Forms.TabPage()
+            Me.TabPage2 = New System.Windows.Forms.TabPage()
+            Me.pctSurface = New System.Windows.Forms.PictureBox()
+            Me.ToolStripContainer1.ContentPanel.SuspendLayout()
+            Me.ToolStripContainer1.SuspendLayout()
+            CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SplitContainer1.Panel1.SuspendLayout()
+            Me.SplitContainer1.Panel2.SuspendLayout()
+            Me.SplitContainer1.SuspendLayout()
+            CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SplitContainer2.SuspendLayout()
+            Me.TabControl1.SuspendLayout()
+            Me.TabPage2.SuspendLayout()
+            CType(Me.pctSurface, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            '
+            'MenuStrip1
+            '
+            Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+            Me.MenuStrip1.Name = "MenuStrip1"
+            Me.MenuStrip1.Size = New System.Drawing.Size(970, 24)
+            Me.MenuStrip1.TabIndex = 0
+            Me.MenuStrip1.Text = "MenuStrip1"
+            '
+            'ToolStripContainer1
+            '
+            '
+            'ToolStripContainer1.ContentPanel
+            '
+            Me.ToolStripContainer1.ContentPanel.Controls.Add(Me.SplitContainer1)
+            Me.ToolStripContainer1.ContentPanel.Size = New System.Drawing.Size(970, 378)
+            Me.ToolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.ToolStripContainer1.Location = New System.Drawing.Point(0, 24)
+            Me.ToolStripContainer1.Name = "ToolStripContainer1"
+            Me.ToolStripContainer1.Size = New System.Drawing.Size(970, 403)
+            Me.ToolStripContainer1.TabIndex = 1
+            Me.ToolStripContainer1.Text = "ToolStripContainer1"
+            '
+            'SplitContainer1
+            '
+            Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+            Me.SplitContainer1.Name = "SplitContainer1"
+            '
+            'SplitContainer1.Panel1
+            '
+            Me.SplitContainer1.Panel1.Controls.Add(Me.TabControl1)
+            '
+            'SplitContainer1.Panel2
+            '
+            Me.SplitContainer1.Panel2.Controls.Add(Me.SplitContainer2)
+            Me.SplitContainer1.Size = New System.Drawing.Size(970, 378)
+            Me.SplitContainer1.SplitterDistance = 743
+            Me.SplitContainer1.TabIndex = 0
+            '
+            'SplitContainer2
+            '
+            Me.SplitContainer2.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.SplitContainer2.Location = New System.Drawing.Point(0, 0)
+            Me.SplitContainer2.Name = "SplitContainer2"
+            Me.SplitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal
+            Me.SplitContainer2.Size = New System.Drawing.Size(223, 378)
+            Me.SplitContainer2.SplitterDistance = 168
+            Me.SplitContainer2.TabIndex = 0
+            '
+            'TabControl1
+            '
+            Me.TabControl1.Controls.Add(Me.TabPage1)
+            Me.TabControl1.Controls.Add(Me.TabPage2)
+            Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+            Me.TabControl1.Name = "TabControl1"
+            Me.TabControl1.SelectedIndex = 0
+            Me.TabControl1.Size = New System.Drawing.Size(743, 378)
+            Me.TabControl1.TabIndex = 0
+            '
+            'TabPage1
+            '
+            Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+            Me.TabPage1.Name = "TabPage1"
+            Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+            Me.TabPage1.Size = New System.Drawing.Size(735, 352)
+            Me.TabPage1.TabIndex = 0
+            Me.TabPage1.Text = "Layout Design"
+            Me.TabPage1.UseVisualStyleBackColor = True
+            '
+            'TabPage2
+            '
+            Me.TabPage2.Controls.Add(Me.pctSurface)
+            Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+            Me.TabPage2.Name = "TabPage2"
+            Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+            Me.TabPage2.Size = New System.Drawing.Size(735, 352)
+            Me.TabPage2.TabIndex = 1
+            Me.TabPage2.Text = "3D Preview"
+            Me.TabPage2.UseVisualStyleBackColor = True
+            '
+            'pctSurface
+            '
+            Me.pctSurface.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+            Me.pctSurface.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.pctSurface.Location = New System.Drawing.Point(3, 3)
+            Me.pctSurface.Name = "pctSurface"
+            Me.pctSurface.Size = New System.Drawing.Size(729, 346)
+            Me.pctSurface.TabIndex = 1
+            Me.pctSurface.TabStop = False
+            '
+            'MainForm
+            '
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(970, 427)
+            Me.Controls.Add(Me.ToolStripContainer1)
+            Me.Controls.Add(Me.MenuStrip1)
+            Me.MainMenuStrip = Me.MenuStrip1
+            Me.Name = "MainForm"
+            Me.Text = "3D Dungeon Maker"
+            Me.ToolStripContainer1.ContentPanel.ResumeLayout(False)
+            Me.ToolStripContainer1.ResumeLayout(False)
+            Me.ToolStripContainer1.PerformLayout()
+            Me.SplitContainer1.Panel1.ResumeLayout(False)
+            Me.SplitContainer1.Panel2.ResumeLayout(False)
+            CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.SplitContainer1.ResumeLayout(False)
+            CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.SplitContainer2.ResumeLayout(False)
+            Me.TabControl1.ResumeLayout(False)
+            Me.TabPage2.ResumeLayout(False)
+            CType(Me.pctSurface, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+            Me.PerformLayout()
 
-    End Sub
+        End Sub
     Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
     Friend WithEvents ToolStripContainer1 As System.Windows.Forms.ToolStripContainer
     Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
-    Friend WithEvents pctSurface As System.Windows.Forms.PictureBox
-    Friend WithEvents SplitContainer2 As System.Windows.Forms.SplitContainer
+        Friend WithEvents SplitContainer2 As System.Windows.Forms.SplitContainer
+        Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+        Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+        Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+        Friend WithEvents pctSurface As System.Windows.Forms.PictureBox
 End Class
 
 end namespace
